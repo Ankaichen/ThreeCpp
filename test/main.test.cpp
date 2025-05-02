ddfe4b158555b1536a84586659dcbd9fcb2d9114
ankaichen <2751551364akc@gmail.com>
2025-05-02 15:08:06 +0800

    add google test, glfw, glad

---
+++ test/main.test.cpp
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file           : main.test.cpp
+  * @author         : An Kaichen
+  * @brief          : None
+  * @attention      : None
+  * @date           : 25-5-2
+  ******************************************************************************
+  */
+
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[]) {
+
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
