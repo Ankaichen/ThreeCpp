a93cd433624d2236f34400e0fffdf3999af031d6
ankaichen <2751551364akc@gmail.com>
2025-05-04 00:54:17 +0800

    add fwd

---
+++ src/core/core_fwd.h
@@ -0,0 +1,27 @@
+/**
+  ******************************************************************************
+  * @file           : core_fwd.h
+  * @author         : An Kaichen
+  * @brief          : None
+  * @attention      : None
+  * @date           : 25-5-3
+  ******************************************************************************
+  */
+
+#ifndef THREECPP_CORE_FWD_H
+#define THREECPP_CORE_FWD_H
+
+namespace three {
+    class TypeIdProvider;
+
+    template<typename Derived>
+    class InstanceIdProvider;
+
+    class Event;
+
+    class EventDispatcher;
+
+    class Object3D;
+}
+
+#endif //THREECPP_CORE_FWD_H
