94005eb755410c8e209fe432be469639e978a1bc
ankaichen <2751551364akc@gmail.com>
2025-05-04 00:50:32 +0800

    add glm for math

---
+++ src/math/math.h
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file           : math.h
+  * @author         : An Kaichen
+  * @brief          : None
+  * @attention      : None
+  * @date           : 25-5-3
+  ******************************************************************************
+  */
+
+#ifndef THREECPP_MATH_H
+#define THREECPP_MATH_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+namespace three::math {
+    using namespace ::glm;
+    using euler = vec3;
+}
+
+#endif //THREECPP_MATH_H
