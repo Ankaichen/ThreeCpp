cb1072059014df9eb152a429bf7ba7f8257218d7
ankaichen <2751551364akc@gmail.com>
2025-05-04 00:48:35 +0800

    add comment

--- src/core/event_dispatcher.h
+++ src/core/event_dispatcher.h
@@ -2,8 +2,14 @@
   ******************************************************************************
   * @file           : event_dispatcher.h
   * @author         : An Kaichen
-  * @brief          : None
-  * @attention      : None
+  * @brief          : Provides the Event and EventDispatcher classes for event handling and listener management.
+  *                   The `Event` class serves as a base class for creating specific events, while the
+  *                   `EventDispatcher` class allows for managing event listeners and dispatching events to them.
+  *                   Listeners are callback functions that are triggered when a specific event is dispatched.
+  * @attention      : The `Event` class should be subclassed to create custom events. The `EventDispatcher`
+  *                   class manages listeners (callbacks) for those events and provides functions to add,
+  *                   remove, and count listeners. The event system relies on `TypeId` to uniquely identify
+  *                   different event types.
   * @date           : 25-5-2
   ******************************************************************************
   */
@@ -20,6 +26,9 @@
 
 namespace three {
 
+    /**
+     * Event base class, inherited in subclasses, used to define specific events
+     */
     class Event : public TypeIdProvider {
     public:
         Event() = default;
@@ -29,8 +38,14 @@ namespace three {
         InitTypeId(Event);
     };
 
+    /**
+     * This class allows to dispatch event and run callback function (Listener).
+     */
     class EventDispatcher {
     public:
+        /**
+         * The type of callback function.
+         */
         using Listener = std::function<void(const std::shared_ptr<Event> &)>;
         using EventTypeId = TypeId;
     public:
@@ -38,12 +53,30 @@ namespace three {
 
         virtual ~EventDispatcher() noexcept = 0;
 
+        /**
+         * Add a Listener to the event of eventId.
+         * @param eventId the TypeId of event.
+         * @param listener the callback function set to event.
+         */
         void addEventListener(EventTypeId eventId, const Listener &listener);
 
+        /**
+         * Get the count of Listener of an event.
+         * @param eventId the TypeId of event.
+         * @return the count of Listener.
+         */
         std::size_t getListenerCount(EventTypeId eventId) const;
 
+        /**
+         * Clear all Listeners of an event.
+         * @param eventId the TypeId of event.
+         */
         void clearEventListener(EventTypeId eventId);
 
+        /**
+         * Dispatches an event object.
+         * @param event an event object
+         */
         void dispatchEvent(const std::shared_ptr<Event> &event) const;
 
     private:
--- src/core/typeid_provider.h
+++ src/core/typeid_provider.h
@@ -2,8 +2,12 @@
   ******************************************************************************
   * @file           : typeid_provider.h
   * @author         : An Kaichen
-  * @brief          : None
-  * @attention      : None
+  * @brief          : Provides a mechanism for obtaining unique type identifiers (TypeId) for classes.
+  *                   The TypeId is used to differentiate types at runtime, which is particularly useful
+  *                   for dynamic type identification or type comparison in generic code.
+  * @attention      : The `TypeIdProvider` class is intended to be inherited by other classes.
+  *                   The macro `InitTypeId(class_name)` should be used in the derived class to implement
+  *                   the `getTypeId()` function, which will return the unique type identifier for that class.
   * @date           : 25-5-2
   ******************************************************************************
   */
@@ -15,20 +19,37 @@
 
 namespace three {
 
+    /**
+     * Unique ID for each type
+     */
     using TypeId = intptr_t;
 
+    /**
+     * Provides an interface for obtaining TypeId
+     */
     class TypeIdProvider {
     public:
         TypeIdProvider() = default;
 
         virtual ~TypeIdProvider() noexcept = 0;
 
+        /**
+         * Function for obtaining TypeId
+         * In the subclass, use the InitTypeId(class_name) macro to rewrite this function
+         * @return TypeId
+         */
         [[nodiscard]] virtual TypeId getTypeId() const = 0;
 
+        /**
+         * A template function used to generate a TypeId for each type,
+         * which can be used to obtain the TypeId by class name
+         * @tparam Derived class name
+         * @return TypeId of Derived
+         */
         template<typename Derived>
-        static intptr_t getTypeId() {
-            const static int8_t _{0};
-            return reinterpret_cast<TypeId>(&_);
+        inline static intptr_t getTypeId() {
+            const static int8_t dummy{0};
+            return reinterpret_cast<TypeId>(&dummy);
         }
     };
 
